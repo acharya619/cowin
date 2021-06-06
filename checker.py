fda22dda0df700a5f8d6759432942c5b739c8ed9
Shubham Acharya <shubham.acharya@oracle.com>
2021-06-06 12:27:38 +0530

    new commands added

--- checker.py
+++ checker.py
@@ -3,7 +3,7 @@ import json
 from datetime import date
 import logging, time
 
-bot_token = '1700150990:AAG4_6fBExxipHYJRwTTBzkBIVKmV0OJzhU'
+bot_token = '1882701276:AAGxT6p7YTFqhWZTxkbQmBKWAnp-kvdhLOk'
 
 chat_id_list = []
 offset = 0
@@ -49,9 +49,7 @@ def fetchStatesList():
 
 def fetchDistrictList(st_code):
     path = "/v2/admin/location/districts/" + str(st_code)
-    #print(formUri(path))
     response = requests.get(formUri(path), headers={"Accept-Language": "hi_IN", "User-Agent": "PostmanRuntime/7.28.0"})
-    #print(response.content)
     return json.loads(response.content)['districts']
 
 def build_district_list(district_list):
@@ -61,22 +59,28 @@ def build_district_list(district_list):
     #print(msg_bundle)
     return msg_bundle
 
+def build_state_list(state_list):
+    msg_bundle = '*_State Name - Code_*\n---------------------------------------\n'
+    for state in states_list:
+        msg_bundle += (state['state_name'] + " - " + str(state['state_id']) + '\n')
+    return msg_bundle
+
 def calenderByDistrict(districtCode):
     path = "/v2/appointment/sessions/public/calendarByDistrict"
     params = {"district_id": districtCode, "date": date}
     response = requests.get(formUri(path), params=params, headers=headers)
-    logger.info('Got response - calenderByDistrict')
-    logger.info(response.status_code)
+    logger.debug('Got response - calenderByDistrict')
+    logger.debug(response.status_code)
     #logger.info(response.content)
     return json.loads(response.content)
 
 def send_msg(bot_message, chat_id):
-    for spchar in ['(', ')', '{', '}', '-', '.', ',', ':']:
+    for spchar in ['(', ')', '{', '}', '-', '.', ',', ':', '!']:
         bot_message = bot_message.replace(spchar, "\\" + spchar)
     uri = 'https://api.telegram.org/bot' + bot_token + '/sendMessage?chat_id=' + str(chat_id) + '&parse_mode=MarkdownV2&text=' + bot_message
     #print(uri, bot_message)
     response = requests.get(uri)
-    #print(response.content)
+    logger.debug(json.loads(response.content))
     return json.loads(response.content)
 
 def build_msg(name, pincode, fee, capacity, age_limit, vaccine, slot_list, dose1_cap, dose2_cap):
@@ -94,19 +98,20 @@ def addToChatList(chatId, searchType, code, only45Plus, onlyAvailable):
             if str(chat['code']) != str(code):
                 chat['code'] = code
                 changed = True
-            if chat['only45Plus'] != only45Plus.upper():
+            if chat['only45Plus'].upper() != only45Plus.upper():
                 chat['only45Plus'] = only45Plus.upper()
                 changed = True
-            if chat['onlyAvailable'] != onlyAvailable.upper():
+            if chat['onlyAvailable'].upper() != onlyAvailable.upper():
                 chat['onlyAvailable'] = onlyAvailable.upper()
                 changed = True
             if changed:
-                send_msg("Alert type has been changed to \nBased on: " + searchType + ", District code: " + code + ", 18+ Only: " + only45Plus, ", Only Available Slots: " + onlyAvailable, chatId)
+                send_msg("*Alert type has been changed to* \nBased on: " + searchType + ", District code: " + code + ", 18+ Only: " + only45Plus + ", Only Available Slots: " + onlyAvailable, chatId)
                 return True
             else:
                 send_msg("Alerts are already configured with given settings, Nothing Changed!", chatId)
                 return False
     chat_id_list.append({"id": chatId, "type": searchType, "code": code, "only45Plus": only45Plus, "onlyAvailable": onlyAvailable})
+    logger.info("New Alert registered: " + str(chat_id_list))
     send_msg("*Alert has been added*\n Based on: " + searchType + ", District code: " + code + ", 18+ Only: " + only45Plus + ", Only Available Slots: " + onlyAvailable, chatId)
     return True
 
@@ -120,8 +125,38 @@ def isCodeInDistrictList(dist_code, district_list):
     for dist in district_list:
         if str(dist['district_id']) == dist_code:
             return True
-    return False       
+    return False     
 
+def removeChatId(chatToremove):
+    for chat in chat_id_list:  
+        if chatToremove['id'] == chat['id']:
+            chat_id_list.remove(chat)
+            logger.info("Alert removed: " + str(chat_id_list))
+            send_msg("Alerts Stopped", chatToremove['id'])
+            
+def showStatus(chat):
+    for c in chat_id_list:  
+        if chat['id'] == c['id']:
+            searchType = c['type']
+            code = c['code']
+            only45Plus = c['only45Plus']
+            onlyAvailable = c['onlyAvailable']
+            send_msg("*Alert are enabled with:*\n Based on: " + searchType + ", District code: " + code + ", 18+ Only: " + only45Plus + ", Only Available Slots: " + onlyAvailable, chat['id'])
+            return True
+    send_msg("Alerts are not enabled", chat['id'])
+    return False
+
+def showHelp(chat):
+    help_msg = "*_Command and Usage\n\n_*"
+    help_msg += "*_help_* : show help text\n\n"
+    help_msg += "*_states_* : show states with state codes\n\n"
+    help_msg += "*_districts_*-{state code} : Show help text\n\n"
+    help_msg += "*_status_* : Show alert status\n\n"
+    help_msg += "*_district-{state code}-{district code}-{Only 18-44 Age vaccines}-{Only available slots}_* : request to enable alerts districtwise\n\n"
+    help_msg += "_NOTE: Alert enabling request should follow str-int-int-[yes/no]-[yes/no] format._"
+    send_msg(help_msg, chat['id'])
+    
+    
 def update_chat_list():
     try:
         global offset, states_list
@@ -130,7 +165,6 @@ def update_chat_list():
         params = {
             "offset": offset
         }
-        #print(uri)
         response = requests.get(uri, params=params, headers=headers)
         updates = json.loads(response.content)
         updates_msg_list = updates["result"]
@@ -140,19 +174,29 @@ def update_chat_list():
             latest_update_offset = update['update_id']
             update = update['message']
             chat = update['chat']
-            #print(chat)
             txt = update['text']
-            #print(txt)
             command = txt.split('-')
-            #print(command)
-            if len(command) == 5:
+            if len(command) == 1:
+                if command[0].lower() == 'stop':
+                    removeChatId(chat)
+                elif command[0].lower() == 'status':
+                    showStatus(chat)
+                elif command[0].lower() == 'help':
+                    showHelp(chat)
+                elif command[0].lower() == 'states':
+                    send_msg(build_state_list(states_list), chat['id'])
+                else:
+                    send_msg("Not a valid format, Alerts not enabled.", chat['id'])
+            elif len(command) == 2 and command[0].lower() == 'districts':
+                district_list = fetchDistrictList(command[1])
+                send_msg(build_district_list(district_list), chat['id'])
+            elif len(command) == 5:
                 if command[0].lower() == "district":
                     try:
                         st_code = command[1]
                         dist_code = command[2]
                         only45Plus = command[3]
                         onlyAvailable = command[4]
-                        #print(states_list)
                         if isCodeInStateList(st_code, states_list):
                             district_list = fetchDistrictList(st_code)
                             if isCodeInDistrictList(dist_code, district_list):
@@ -167,12 +211,11 @@ def update_chat_list():
                                     send_msg("Choose [Only 18+ Alerts] parameter as Yes or No", chat['id'])
                             else:
                                 logger.error("Not a valid district")
-                                send_msg("Not a valid district code, Alerts not enabled.", chat['id'])
-                                #dist_lis_str = json.dumps(district_list)
-                                #print(dist_lis_str)
+                                send_msg("Not a valid district code, Alerts not enabled. Choose a valid code.", chat['id'])
                                 send_msg(build_district_list(district_list), chat['id'])
                         else:
-                            send_msg("Not a valid state code, Alerts not enabled.", chat['id'])
+                            send_msg("Not a valid state code, Alerts not enabled. Choose a valid code.", chat['id'])
+                            send_msg(build_state_list(states_list), chat['id'])
                     except Exception as e:
                         logger.error(e)
                 else:
@@ -209,40 +252,36 @@ def parse_json_response(response, chat):
             if (chat['onlyAvailable'].upper() == 'Y' or chat['onlyAvailable'].upper() == 'YES') and capacity > '0':
                 if (chat['only45Plus'].upper() == 'Y' or chat['only45Plus'].upper() == 'YES') and age_limit < '45':
                     msg = build_msg(name, pincode, fee, capacity, age_limit, vaccine, slot_list, dose1_cap, dose2_cap)
-                    logger.info(send_msg(msg, chat['id']))
                 elif chat['only45Plus'].upper() == 'N' or chat['only45Plus'].upper() == 'NO':
                     msg = build_msg(name, pincode, fee, capacity, age_limit, vaccine, slot_list, dose1_cap, dose2_cap)
-                    logger.info(send_msg(msg, chat['id']))  
             elif chat['onlyAvailable'].upper() == 'N' or chat['onlyAvailable'].upper() == 'NO':
                 if (chat['only45Plus'].upper() == 'Y' or chat['only45Plus'].upper() == 'YES') and age_limit < '45':
                     msg = build_msg(name, pincode, fee, capacity, age_limit, vaccine, slot_list, dose1_cap, dose2_cap)
-                    logger.info(send_msg(msg, chat['id']))
                 elif chat['only45Plus'].upper() == 'N' or chat['only45Plus'].upper() == 'NO':
                     msg = build_msg(name, pincode, fee, capacity, age_limit, vaccine, slot_list, dose1_cap, dose2_cap)
-                    logger.info(send_msg(msg, chat['id']))  
                       
                 
             
     
 if __name__ == "__main__":
     try:
+        logger.info("...Service started...")
         states_list = fetchStatesList()
-        limit = 15
+        limit = 2
         while True:
             strt_time = time.time()
-            #print(chat_id_list)
+            logger.debug(str(chat_id_list) if chat_id_list != [] else "No Chat IDs found")
             update_chat_list()
             for chat in chat_id_list:
-                #print(chat)
                 chat_id = chat['id']
                 if chat['type'] == "district":
                     response = calenderByDistrict(chat['code'])
                     parse_json_response(response, chat) 
                 elif chat['type'] == "pincode":
+                    send_msg("_Coming soon...!_", chat_id)
                     pass
             curr_time = time.time()
             if (curr_time - strt_time) < limit:
-                logger.info("sleeping for %s seconds" %(limit - (curr_time - strt_time)))
                 time.sleep(limit - (curr_time - strt_time))
     except Exception as e:
         logger.error(e) 
